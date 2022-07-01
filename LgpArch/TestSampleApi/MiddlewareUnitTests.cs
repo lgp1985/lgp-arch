2ce55cd21126980d8e90fb8b59c11461d7577852
Luis Gustavo Pereira <lgp1985@outlook.com>
2022-07-01 16:18:49 -0300

    covering different use cases

--- LgpArch/TestSampleApi/MiddlewareUnitTests.cs
+++ LgpArch/TestSampleApi/MiddlewareUnitTests.cs
@@ -1,47 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
 using System.Net;
 
 namespace TestSampleApi
 {
+    /// <summary>
+    /// These tests are created with below docs as reference
+    /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/migration/50-to-60-samples?view=aspnetcore-6.0#project-file-csproj"/>
+    /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/core/test/middleware?view=aspnetcore-6.0#send-requests-with-httpclient"/>
+    /// </summary>
     [TestClass]
     public class MiddlewareUnitTests
     {
+        public static HttpClient Client { get { return new WebApplicationFactory<Program>().CreateClient(); } }
+
         [TestMethod]
-        public async Task CheckIfAuthenticationIsInPlaceAsync()
+        public async Task CheckUnauthorizedRouteAsync()
         {
-            // Arrange
-
-            var application = new WebApplicationFactory<Program>()
-                .WithWebHostBuilder(builder =>
-                {
-                    builder.UseTestServer();
-                });
-
-            var client = application.CreateClient();
-
-            // Act / Assert
-
-            await client.CheckStatusForPath("/WeatherForecast", HttpStatusCode.Unauthorized);
-            await client.CheckStatusForPath("/", HttpStatusCode.NotFound);
+            await Client.CheckStatusForPath("/WeatherForecast", HttpStatusCode.Unauthorized);
         }
 
+        [TestMethod]
+        public async Task CheckNotFoundRouteAsync()
+        {
+            await Client.CheckStatusForPath("/", HttpStatusCode.NotFound);
+        }
+
+        [TestMethod]
+        public async Task CheckOKRouteAsync()
+        {
+            await Client.CheckStatusForPath("/swagger/v1/swagger.json", HttpStatusCode.OK);
+        }
     }
-    public static class Extensions{
+    public static class Extensions
+    {
         public static async Task CheckStatusForPath(this HttpClient client, string route, HttpStatusCode expectedStatus)
         {
-            await Assert.ThrowsExceptionAsync<HttpRequestException>(async () =>
-            {
-                try
-                {
-                    var response = await client.GetStringAsync(route);
-                }
-                catch (HttpRequestException ex)
-                {
-                    Assert.AreEqual(expectedStatus, ex.StatusCode);
-                    throw;
-                }
-            });
+            var response = await client.GetAsync(route);
+            Assert.AreEqual(expectedStatus, response.StatusCode);
         }
     }
 }
