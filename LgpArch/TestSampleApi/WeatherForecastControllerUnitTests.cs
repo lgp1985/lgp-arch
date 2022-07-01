e18771a63b8d01d645fd7879cf6f65c455ff0a33
Luis Gustavo Pereira <lgp1985@outlook.com>
2022-07-01 16:27:42 -0300

    iterate results

--- LgpArch/TestSampleApi/WeatherForecastControllerUnitTests.cs
+++ LgpArch/TestSampleApi/WeatherForecastControllerUnitTests.cs
@@ -15,4 +15,23 @@ public class WeatherForecastControllerUnitTests
         // Assert
         Assert.AreEqual(5, response.Count());
     }
+
+    [TestMethod]
+    public void ResponseContainsValuesResults()
+    {
+        // Arrange
+        var weatherForecast = new WeatherForecastController(new NullLogger<WeatherForecastController>());
+
+        // Act
+        var response = weatherForecast.Get();
+
+        // Assert
+        foreach (var item in response)
+        {
+            Assert.IsTrue(item.TemperatureC != item.TemperatureF);
+            Assert.IsNotNull(item.Summary);
+            Assert.IsNotNull(item.Date);
+            Assert.IsTrue(DateTime.MinValue < item.Date);
+        }
+    }
 }
