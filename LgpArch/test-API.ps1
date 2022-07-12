8096b0fc5d09370ce09eda66ac0f764859f21c01
Luis Gustavo Pereira <lgp1985@outlook.com>
2022-07-11 22:52:44 -0300

    using HTTP/2

--- LgpArch/test-API.ps1
+++ LgpArch/test-API.ps1
@@ -7,4 +7,13 @@ Start-Process "https://jwt.ms/#access_token=$($jwt.AccessToken)"
 
 $jwt.AccessToken | Set-Clipboard
 
-Invoke-RestMethod -Headers @{Authorization = "$($jwt.TokenType) $($jwt.AccessToken)" } -Uri  "https://localhost:49153/WeatherForecast" -Method Get
+# Invoke-RestMethod -Headers @{Authorization = "$($jwt.TokenType) $($jwt.AccessToken)" } -Uri  "https://localhost:49153/WeatherForecast" -Method Get
+$hc = [System.Net.Http.HttpClient]::new()
+$hc.DefaultRequestVersion = [System.Net.HttpVersion]::Version30
+$hc.DefaultRequestHeaders.Add('Authorization', "$($jwt.TokenType) $($jwt.AccessToken)")
+$r = $hc.GetAsync('https://localhost:49157/WeatherForecast').GetAwaiter().GetResult()
+$r
+Write-Host 'Headers'
+$r.Content.Headers
+Write-Host 'Content'
+$r.Content.ReadAsStringAsync().GetAwaiter().GetResult()
